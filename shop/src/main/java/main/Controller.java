4abc8912784821943b4c33f7c21fdb3c7d154373
VladimirCunichin <v.cunichin98@gmail.com>
2020-03-04 13:51:03 +0200

    Add files via upload

--- shop/src/main/java/main/Controller.java
+++ shop/src/main/java/main/Controller.java
@@ -1,5 +1,7 @@
 package main;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import model.Article;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,24 +14,48 @@ import org.springframework.web.bind.annotation.RestController;
 public class Controller {
     @Autowired
     private Service articleService;
-    @RequestMapping("/articles")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get all articles"),
+            @ApiResponse(code = 404, message = "No articles found")
+    })
+    @GetMapping("/articles")
     public List<Article> getAllArticles(){
         return articleService.getAllArticles();
     }
-    @RequestMapping("/articles/{id}")
+
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Get article by it's Id"),
+            @ApiResponse(code = 404, message = "article not found")
+    })
+    @GetMapping("/articles/{id}")
     public Article getArticle(@PathVariable String id){
         return articleService.getArticle(id);
 
     }
-    @RequestMapping(method = RequestMethod.POST, value = "/articles")
+    @ApiResponses( value = {
+            @ApiResponse(code = 201, message = "Successfully added article"),
+            @ApiResponse(code = 400, message = "article already exists"),
+            @ApiResponse(code = 404, message = "NOT FOUND"),
+            @ApiResponse(code = 500, message = "Failed to add an article"),
+    })
+    @PostMapping( value = "/articles")
     public void addArticle(@RequestBody Article article){
         articleService.addArticle(article);
     }
-    @RequestMapping(method = RequestMethod.PUT, value = "/articles/{id}")
+    @ApiResponses( {
+            @ApiResponse(code = 200, message = "Successfully updated article"),
+            @ApiResponse(code = 400, message = "Bad Request"),
+            @ApiResponse(code = 404, message = "article not found"),
+    })
+    @PutMapping(value = "/articles/{id}")
     public void updateArticle(@RequestBody Article article,@PathVariable String id){
         articleService.updateArticle(article, id);
     }
-    @RequestMapping(method = RequestMethod.DELETE, value = "/articles/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "article deleted by Id"),
+            @ApiResponse(code = 404, message = "article not found")
+    })
+    @DeleteMapping( value = "/articles/{id}")
     public void deleteArticle(@PathVariable String id){
         articleService.deleteArticle(id);
     }
